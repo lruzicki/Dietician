7dcaff70b10197322375ab47f9cb849ae070173e
lukruz <56487722+lukruz@users.noreply.github.com>
2022-02-15 18:57:31 +0100

    Add files via upload

---
+++ Data/DieticianContext.cs
@@ -0,0 +1,27 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Dietician;
+using Dietician.Models;
+
+namespace Dietician.Data
+{
+    public class DieticianContext : DbContext
+    {
+        public DbSet<Dietician.Models.Chef> Chefs { get; set; }
+        public DbSet<MvcMovie.Dietician.Recipe> Recipe { get; set; }
+
+        public DieticianContext (DbContextOptions<DieticianContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Dietician.Models.Publication> Publication { get; set; }
+
+        public DbSet<Dietician.Models.ProjectRole> ProjectRole { get; set; }
+
+    }
+}
