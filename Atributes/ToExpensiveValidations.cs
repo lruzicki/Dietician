44b68a98f8d0d9ca630a9505198f2c1ab470a3e5
lukruz <56487722+lukruz@users.noreply.github.com>
2022-02-15 18:58:10 +0100

    Add files via upload

---
+++ Atributes/ToExpensiveValidations.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dietician.Atributes
+{
+    public class ToExpensiveValidations :ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime birthday = (DateTime)value;
+            if (birthday.Year < 1900)
+                return new ValidationResult("Surely food is that old?");
+            if (birthday.Year > 2022)
+                return new ValidationResult("Recipe from future!");
+            if (birthday.Month == 7)
+                return new ValidationResult("...July? And over 50?");
+            return ValidationResult.Success;
+        }
+    }
+}
