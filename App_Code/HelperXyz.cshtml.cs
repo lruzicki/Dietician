b2c1cc746417452eb9e80b9a67ff7d0902a9f3cf
lukruz <56487722+lukruz@users.noreply.github.com>
2022-02-15 18:58:21 +0100

    Add files via upload

---
+++ App_Code/HelperXyz.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dietician.App_Code
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
